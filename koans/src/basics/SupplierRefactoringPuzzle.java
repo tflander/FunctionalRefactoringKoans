f1a407db04cfd1f0168677bb14a6c5c0cf311773
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-09-19 10:08:58 -0400

    todo rather than comments for refactoring puzzle

--- koans/src/basics/SupplierRefactoringPuzzle.java
+++ koans/src/basics/SupplierRefactoringPuzzle.java
@@ -5,12 +5,16 @@ import com.sandwich.util.Assert;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import static com.sandwich.koan.constant.KoanConstants.__;
 import static com.sandwich.util.Assert.assertEquals;
+import static com.sandwich.util.Assert.fail;
 
 public class SupplierRefactoringPuzzle {
+
+    Consumer<String> todo = Assert::fail;
 
     static abstract class Animal {
         public abstract String speak();
@@ -32,19 +36,13 @@ public class SupplierRefactoringPuzzle {
         }
     }
 
-    /**
-     * TODO: Once you get the asserts to pass by plugging in the correct expectation,
-     * TODO: refactor the code to use a supplier instead of polymorphism.
-     * TODO: Delete the Cow and Pig sub-classes, then make Animal not abstract.
-     * TODO: Have a constructor for animal that takes a speakFunction.  Use that function in the speak method
-     */
     @Koan
     public void codeToRefactor() {
-
+        todo.accept("Refactor the code to use a supplier instead of polymorphism.\nDelete the Cow and Pig sub-classes, then make Animal not abstract.\nHave a constructor for animal that takes a speakFunction.  Use that function in the speak method.\nDon't forget to delete this 'todo' line.");
         Animal cow = new Cow();
         Animal pig = new Pig();
-        assertEquals(__, cow.speak());
-        assertEquals(__, pig.speak());
+        assertEquals("moo", cow.speak());
+        assertEquals("oink", pig.speak());
     }
 
 }
