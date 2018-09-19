9100ea67dcb75b9b491f74541f9e05f8e57e062c
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-09-19 19:38:34 -0400

    added about lazyness

---
+++ koans/src/concepts/AboutLazyness.java
@@ -0,0 +1,63 @@
+package concepts;
+
+import com.sandwich.koan.Koan;
+import com.sandwich.util.Assert;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import static com.sandwich.util.Assert.assertFalse;
+import static com.sandwich.util.Assert.assertTrue;
+
+public class AboutLazyness {
+
+    Consumer<String> todo = Assert::fail;
+
+    private boolean slowDataCalled;
+    private boolean slowDataUsed;
+
+    private List<Integer> numbers = Arrays.asList(2, 4, 6, 8);
+
+    @Koan
+    public void eager() {
+        slowDataCalled = false;
+        slowDataUsed = false;
+
+        processNumbers(numbers, slowDataCall());
+        todo.accept("Contemplate why we made the slow data call, but didn't use the data.\nDelete this line to continue.");
+        assertTrue(slowDataCalled);
+        assertFalse(slowDataUsed);
+    }
+
+    @Koan
+    public void functionalLazyness() {
+        slowDataCalled = false;
+        slowDataUsed = false;
+
+        processNumbersLazy(numbers, () -> slowDataCall());
+        todo.accept("Understand why we didn't have to make the call to get the slow data.\nDelete this line to continue.");
+        assertFalse(slowDataCalled);
+        assertFalse(slowDataUsed);
+    }
+
+    private String slowDataCall() {
+        this.slowDataCalled = true;
+        return "foo";
+    }
+
+    private void processNumbers(List<Integer> numbers, String slowDataCall) {
+        if (numbers.stream().mapToInt(i -> i).sum() > 30) {
+            slowDataUsed = true;
+        }
+    }
+
+    private void processNumbersLazy(List<Integer> numbers, Supplier<String> slowDataSupplier) {
+        if (numbers.stream().mapToInt(i -> i).sum() > 30) {
+            slowDataSupplier.get();
+            slowDataUsed = true;
+        }
+    }
+
+}
